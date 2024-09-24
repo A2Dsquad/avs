17665def0f71dd94d2858bfb38b6027b9e2d4a55
lacsomot <hbnguyen.work@gmail.com>
2024-09-24 16:03:20 +0700

    update operator node

--- operator/start.go
+++ operator/start.go
@@ -2,12 +2,17 @@ package operator
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/spf13/cobra"
 
 	"go.uber.org/zap"
 
+	// For now using eigentypes
+	eigentypes "github.com/Layr-Labs/eigensdk-go/types"
+
 	aptos "github.com/aptos-labs/aptos-go-sdk"
+	"github.com/aptos-labs/aptos-go-sdk/bcs"
 )
 
 func Start(logger *zap.Logger) *cobra.Command {
@@ -50,4 +55,131 @@ func AptosClient(networkConfig aptos.NetworkConfig) *aptos.Client {
 		panic("Failed to create client:" + err.Error())
 	}
 	return client
+}
+
+func NewOperator(networkConfig aptos.NetworkConfig, config OperatorConfig, aptosConfig string) (*Operator, error) {
+	operator_account, err := SignerFromConfig(aptosConfig)
+	if err != nil {
+		panic("Failed to create operator account:" + err.Error())
+	}
+	client, err := aptos.NewClient(networkConfig)
+	if err != nil {
+		panic("Failed to create client:" + err.Error())
+	}
+
+	// Get operator Status
+	registered := IsOperatorRegistered(client, config.AvsAddress, config.OperatorAddress.String())
+
+	if !registered {
+		log.Println("Operator is not registered with A2D Oracle AVS, registering...")
+
+		quorumNumbers := []byte{0}
+
+		// Register Operator
+		// TODO: minh help
+		// ignore error here because panic all the time
+		_ = RegisterOperator(client, operator_account, config.AvsAddress.String(), quorumNumbers, PubkeyRegistrationParams{})
+	}
+
+	// connect to aggregator
+	// NewAggregatorRpcClient()
+
+	// Get OperatorId
+	operatorId := eigentypes.OperatorIdFromKeyPair(config.BlsKeyPair)
+
+	// return Operator
+	operator := Operator{
+		account:    operator_account,
+		operatorId: operatorId,
+		avsAddress: config.AvsAddress,
+		// TODO: set aggregator here
+		AggRpcClient: nil,
+	}
+	return &operator, nil
+}
+
+func IsOperatorRegistered(client *aptos.Client, contract aptos.AccountAddress, operator_addr string) bool {
+	payload := &aptos.ViewPayload{
+		Module: aptos.ModuleId{
+			Address: contract,
+			Name:    "registry_coordinator",
+		},
+		Function: "get_operator_status",
+		ArgTypes: []aptos.TypeTag{},
+		Args:     [][]byte{[]byte(operator_addr)},
+	}
+
+	vals, err := client.View(payload)
+	if err != nil {
+		panic("Could not get operator status:" + err.Error())
+	}
+	status := vals[0].(uint8)
+	return status == 0
+}
+
+// quorum_numbers: vector<u8>, operator: &signer, params: bls_apk_registry::PubkeyRegistrationParams
+func RegisterOperator(
+	client *aptos.Client,
+	operator_account *aptos.Account,
+	contract_addr string,
+	quorum_numbers []byte,
+	bls_register_params PubkeyRegistrationParams,
+) error {
+	contract := aptos.AccountAddress{}
+	err := contract.ParseStringRelaxed(contract_addr)
+	if err != nil {
+		panic("Failed to parse address:" + err.Error())
+	}
+	quorum, err := bcs.SerializeBytes(quorum_numbers)
+	if err != nil {
+		panic("Failed to bcs serialize quorum:" + err.Error())
+	}
+	params, err := bcs.Serialize(&bls_register_params)
+	if err != nil {
+		panic("Failed to bcs serialize bls_register_params:" + err.Error())
+	}
+	payload := aptos.EntryFunction{
+		Module: aptos.ModuleId{
+			Address: contract,
+			Name:    "registry_coordinator",
+		},
+		Function: "registor_operator",
+		ArgTypes: []aptos.TypeTag{},
+		Args: [][]byte{
+			quorum, operator_account.Signer.AuthKey().Bytes(), params,
+		},
+	}
+	// Build transaction
+	rawTxn, err := client.BuildTransaction(operator_account.AccountAddress(),
+		aptos.TransactionPayload{Payload: &payload})
+	if err != nil {
+		panic("Failed to build transaction:" + err.Error())
+	}
+
+	// Sign transaction
+	signedTxn, err := rawTxn.SignedTransaction(operator_account)
+	if err != nil {
+		panic("Failed to sign transaction:" + err.Error())
+	}
+	fmt.Printf("Submit register operator for %s\n", operator_account.AccountAddress())
+
+	// Submit and wait for it to complete
+	submitResult, err := client.SubmitTransaction(signedTxn)
+	if err != nil {
+		panic("Failed to submit transaction:" + err.Error())
+	}
+	txnHash := submitResult.Hash
+
+	// Wait for the transaction
+	fmt.Printf("And we wait for the transaction %s to complete...\n", txnHash)
+	userTxn, err := client.WaitForTransaction(txnHash)
+	if err != nil {
+		panic("Failed to wait for transaction:" + err.Error())
+	}
+	fmt.Printf("The transaction completed with hash: %s and version %d\n", userTxn.Hash, userTxn.Version)
+	if !userTxn.Success {
+		// TODO: log something more
+		panic("Failed to register operator")
+	}
+	return nil
 }
--- operator/types.go
+++ operator/types.go
@@ -1,5 +1,71 @@
 package operator
 
+import (
+	"net/rpc"
+
+	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
+	aptos "github.com/aptos-labs/aptos-go-sdk"
+	"github.com/aptos-labs/aptos-go-sdk/bcs"
+	"golang.org/x/crypto/ed25519"
+
+	eigentypes "github.com/Layr-Labs/eigensdk-go/types"
+)
+
 type Config struct {
 	CmcApi string `json:"cmc_api"`
+}
+
+type Operator struct {
+	account      *aptos.Account
+    // TODO: change this to aptos-sdk fork
+	operatorId   eigentypes.Bytes32
+	avsAddress   aptos.AccountAddress
+	AggRpcClient *rpc.Client
+}
+type OperatorConfig struct {
+	BlsKeyPair      *bls.KeyPair
+	OperatorAddress aptos.AccountAddress
+	AvsAddress      aptos.AccountAddress
+	AggRpcClient    *rpc.Client
+	// OperatorId           eigentypes.OperatorId
+}
+
+// type OperatorConfig struct {
+// 	// TODO: EcdsaConfig
+
+// }
+
+type BlsConfig struct {
+	KeyPair *bls.KeyPair
+}
+
+type AlternativeSigner struct {
+	privateKey ed25519.PrivateKey
+	publicKey  ed25519.PublicKey
+}
+
+type PubkeyRegistrationParams struct {
+	signature Signature
+	pubkey_g1 PublicKeyWithPoP
+	pubkey_g2 PublicKeyWithPoP
+}
+
+type Signature struct {
+	bytes []byte
+}
+
+type PublicKeyWithPoP struct {
+	bytes []byte
+}
+
+// Implement MarshalBCS
+func (params *PubkeyRegistrationParams) MarshalBCS(ser *bcs.Serializer) {
+	ser.WriteBytes(params.signature.bytes)
+	ser.WriteBytes(params.pubkey_g1.bytes)
+	ser.WriteBytes(params.pubkey_g2.bytes)
+}
+
+type AggregatorRpcClient struct {
+	rpcClient            *rpc.Client
+	aggregatorIpPortAddr string
 }
